bdb13ebed86946699103dc4ba620570dd012591c
hunyoungcha <chamgf5247@naver.com>
2025-03-16 12:01:46 -0400

    ver.2

--- main.c
+++ main.c
@@ -18,10 +18,14 @@ int main(int argc, char* argv[]){
 		int res = pcap_next_ex(pcap, &header, &packet);
 		
 		struct ethernet_header *eth = (struct ethernet_header *)packet;
-		struct ip_header *ip = (struct ip_header *)(packet + sizeof(struct ethernet_header));
-		struct tcp_header *tcp = (struct tcp_header	*)(packet + sizeof(struct ip_header));
-	
-		//출력 함수로 빼기
+		int ethernet_header_length = sizeof(struct ethernet_header); 
+		
+		struct ip_header *ip = (struct ip_header *)(packet + ethernet_header_length);
+		int ip_header_length = (ip->VersionAndIhl & 0x0F) *4;
+		
+		struct tcp_header *tcp = (struct tcp_header	*)(packet + ethernet_header_length + ip_header_length);
+		int tcp_header_length = (tcp->DataOffsetAndReserved >> 4) * 4; 
+
 		printf("======================================================\n");
 		printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
 			eth->srcMac[0], eth->srcMac[1], eth->srcMac[2],
@@ -40,10 +44,6 @@ int main(int argc, char* argv[]){
 		printf("Source Port: %d\n", ntohs(tcp->SourcePort));
 		printf("Destination Port: %d\n", ntohs(tcp->DestinationPort));
 
-		int ethernet_header_length = sizeof(struct ethernet_header); 
-		int ip_header_length = (ip->VersionAndIhl & 0x0F) * 4;        // IHL 값 * 4가 실제 IP 헤더 길이
-		int tcp_header_length = (tcp->DataOffsetAndReserved >> 4) * 4; 
-	
 		const u_char* payload = packet + ethernet_header_length + ip_header_length + tcp_header_length;
 	
 		printf("Payload: ");
